533cd4b33df69227012638450d084d55f18238b8
dfb5361@gmail.com <dfb5361@gmail.com>
2024-07-20 03:20:24 +0900

    파일 구조 변경(나눠진 패키지 Board 패키지로 모음)

--- src/main/java/com/hotdealwork/hotdealwork/board/BoardService.java
+++ src/main/java/com/hotdealwork/hotdealwork/board/BoardService.java
@@ -1,7 +1,5 @@
-package com.hotdealwork.hotdealwork.Service;
+package com.hotdealwork.hotdealwork.board;
 
-import com.hotdealwork.hotdealwork.entity.Board;
-import com.hotdealwork.hotdealwork.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
