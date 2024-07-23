681713bc6c504b320aa9694d53914453abd05c2c
dfb5361@gmail.com <dfb5361@gmail.com>
2024-07-23 18:31:19 +0900

    회원탈퇴 기능 병합, 네비바 로그인 정보 확인 기능 수정

--- src/main/java/com/hotdealwork/hotdealwork/user/UserRepository.java
+++ src/main/java/com/hotdealwork/hotdealwork/user/UserRepository.java
@@ -2,6 +2,7 @@ package com.hotdealwork.hotdealwork.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -10,4 +11,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<SiteUser, Long> {
 
     Optional<SiteUser> findByusername(String username);
+
+    @Transactional
+    void deleteByUsername(String username);
 }
--- src/main/java/com/hotdealwork/hotdealwork/user/UserService.java
+++ src/main/java/com/hotdealwork/hotdealwork/user/UserService.java
@@ -1,8 +1,12 @@
 package com.hotdealwork.hotdealwork.user;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -10,6 +14,7 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final HttpServletRequest request;
 
     public SiteUser create(String username, String email, String password){
         SiteUser user = new SiteUser();
@@ -18,5 +23,21 @@ public class UserService {
         user.setPassword(passwordEncoder.encode(password));
         this.userRepository.save(user);
         return user;
+    }
+
+    public boolean deleteUser(String username, String password) {
+        Optional<SiteUser> userOptional = userRepository.findByusername(username);
+
+        if (userOptional.isPresent()) {
+            SiteUser user = userOptional.get();
+            if (passwordEncoder.matches(password, user.getPassword())) {
+                userRepository.deleteByUsername(username);
+                SecurityContextHolder.clearContext();
+                request.getSession().invalidate();
+                return true;
+            }
+        }
+
+        return false;
     }
 }
