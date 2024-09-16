c5f19d1e142eee7e026c5231c66598048892995f
dfb5361@gmail.com <dfb5361@gmail.com>
2024-09-17 00:42:50 +0900

    게시글 추천 정보 게시글이 아닌 사용자에 기록되록 변경 메인 페이지에 만료되지 않은 최신 글(일주일 이내 작성) 추천 순으로 표시되게 변경

--- src/main/java/com/hotdealwork/hotdealwork/home/homeController.java
+++ src/main/java/com/hotdealwork/hotdealwork/home/homeController.java
@@ -1,13 +1,33 @@
 package com.hotdealwork.hotdealwork.home;
 
+import com.hotdealwork.hotdealwork.board.Board;
+import com.hotdealwork.hotdealwork.board.BoardService;
+import com.hotdealwork.hotdealwork.commu.Commu;
+import com.hotdealwork.hotdealwork.commu.CommuService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 
 @Controller
 public class homeController {
+
+    @Autowired
+    private BoardService boardService;
+
+    @Autowired
+    private CommuService commuService;
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        List<Board> board = boardService.boardMainHot();
+        List<Commu> commu = commuService.commuMainHot();
+
+        model.addAttribute("board", board);
+        model.addAttribute("commu", commu);
 
         return "home";
     }
