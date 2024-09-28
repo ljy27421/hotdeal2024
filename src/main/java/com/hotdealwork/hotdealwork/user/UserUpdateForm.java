63b77cc931a5b71ba56662b7d28fbf2191413cf1
dfb5361@gmail.com <dfb5361@gmail.com>
2024-09-29 00:01:54 +0900

    회원 정보 수정 기능 구현

--- src/main/java/com/hotdealwork/hotdealwork/user/UserUpdateForm.java
+++ src/main/java/com/hotdealwork/hotdealwork/user/UserUpdateForm.java
@@ -4,7 +4,9 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @Data
 @AllArgsConstructor
 public class UserUpdateForm {
