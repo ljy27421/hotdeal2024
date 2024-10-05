9782658724a8603e078c8db48508b55aa3231484
dfb5361@gmail.com <dfb5361@gmail.com>
2024-10-06 01:26:19 +0900

    병합중 오류 수정

--- src/main/java/com/hotdealwork/hotdealwork/board/BoardRepository.java
+++ src/main/java/com/hotdealwork/hotdealwork/board/BoardRepository.java
@@ -4,7 +4,10 @@ package com.hotdealwork.hotdealwork.board;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Integer> {
